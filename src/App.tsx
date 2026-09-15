ae5c26e389ecb3c8aad17226cf20521f311a2be8
CaptainTriton10 <captaintriton167@gmail.com>
2026-09-15 13:03:09 +0100

    feat: add tailwindcss + colour theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <></>;
+  return (
+    <div className="bg-background h-dvh">
+      <h1 className="text-primary text-8xl">James Mather</h1>
+    </div>
+  );
 }
 
 export default App;
